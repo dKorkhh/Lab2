4725b930f087113798e62e4255d52ef91fe6fdd4
Dana Mushchenko <danamushchenko@gmail.com>
2025-04-13 00:37:08 +0300

    list of users

--- Lab2/Exception/FutureBirthDateException.cs
+++ Lab2/Exception/FutureBirthDateException.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Lab2.Exception
 {
     public class FutureBirthDateException : FormatException
     {
-        public FutureBirthDateException() : base("Дата народження не може бути в майбутньому.") { }
+        public FutureBirthDateException() : base("Дата народження не може бути в майбутньому.") { 
+        }
     }
 }
